d985bf992f3b0562cb79a460515df00a15ad6304
gajanan83 <gajanan.java@gmail.com>
2020-06-16 11:55:26 +0000

    second

--- demo1.tf
+++ demo1.tf
@@ -3,8 +3,21 @@ provider "azurerm" {
 }
 resource "azurerm_resource_group" "demo1" {
   name     = "testrg1trainer"
-  location = "eastus"
+  location = "eastus" 
   tags = {
-      env = "resource-group-demo"
-  }
+      env = "testrg1trainer-demo"
+  } 
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name ="terraformrg"
+        storage_account_name = "sa1gaja"
+        container_name = "con1"
+        key = "terraform.tfstate"
+    }
+}
+resource "azurerm_resource_group" "demo2" {
+  name     = "testrg2trainer"
+  location = "southindia"
 }
